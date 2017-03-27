c91512f1f86784c1b8f05611d1ec8ce091f21bb1
Patricia Juarez <patricia@badgermapping.com>
2017-03-28 00:38:40 +0200

    Removed an unneeded comment

--- home/views.py
+++ home/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.conf import settings
 
 
-# Create your views here.
-
 def home_view(request):
     template = 'index.html'
     return render(request, template, {'environment_name': settings.ENVIRONMENT_NAME})
